f349c1b1f5d9deb75e1b2b6c1016731eb8bdf6f0
Allyson Camp <Allysoncamp92@gmail.com>
2023-01-25 22:47:23 -0800

    added url/key/async functions from supabase to fetch-utils.js. completed signup/signin eventlisteners

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
+//imports
+
+
+//dom elemnts
 const signInForm = document.getElementById('sign-in');
 
 const signUpForm = document.getElementById('sign-up');
+
+//state
+
+//events
+
+signUpForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+  
+});
+
+// Fetch 
+
+// Loop
+// O pass object
+// R into render function
+// Append to root dom
+
+
+//display functions
+
+//call necessary functions
 
 // Wire up sign in and sign up forms to supabase
 // Redirect to /other-page on successful auth
--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,6 @@
-// Enter Supabase Information
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+// Enter client Information
+const SUPABASE_URL = 'https://ejeezbcksxazezxketdy.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqZWV6YmNrc3hhemV6eGtldGR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ0ODk2MjcsImV4cCI6MTk5MDA2NTYyN30.uPokWkEqMtvLPS0Xe9q-myZON-BEU1EAO4iq7-o0cEk';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -8,12 +8,54 @@ export function getUser() {
     return client.auth.session() && client.auth.session().user;
 }
 
-export async function signupUser(email, password) {}
+export async function signupUser(email, password) {
+    // const { data, error } = await supabase.auth.signUp({
+    //     email: email,
+    //     password: password,
+    // });
 
-export async function signInUser(email, password) {}
+    // if (error) console.error(error);
+    // return data;
 
-export async function checkAuth() {}
+    const response = await client.auth.signUp({
+        email: email,
+        password: password,
+    });
+    if (error) console.error(error);
+    return response.data;
+}
+
+export async function signInUser(email, password) {
+    // const { data, error } = await client.auth.signInWithPassword({
+    //     email: email,
+    //     password: password,
+    // });
+
+    // if (error) console.error(error);
+    // return data;
+
+    const response = await client.auth.signInWithPassword ({
+        email: email,
+        password: password,
+    });
+    if (error) console.error(error);
+    return response.data;
+}
+
+export async function checkAuth() {
+
+}
 
 export async function redirectIfLoggedIn() {}
 
-export async function logout() {}
+export async function logout() {
+    // const { error } = await client.auth.signOut();
+
+    // if (error) console.error(error);
+    // return error;
+
+    const response = await client.auth.signOut();
+
+    return response.data;
+
+}
