2ef52e59c27b9125d28b9c86be502db64c91f263
Allyson Camp <Allysoncamp92@gmail.com>
2023-01-26 14:43:40 -0800

    final

--- other-page/other-page.js
+++ other-page/other-page.js
@@ -2,7 +2,7 @@ import { checkAuth, logout } from '../fetch-utils.js';
 
 const buttonEl = document.getElementById('logout');
 // use checkAuth function to redirect is user not authenticated
-// checkAuth();
+
 // add event listener to the logout button and call logout
 checkAuth();
 buttonEl.addEventListener('click', () => {
